1894257657ebe30d80f839d4b4599721f2d77346
Meltoz <meltoz@outlook.fr>
2024-12-19 21:14:42 +0100

    Adding preload car on detail

--- app/services/announcement.service.ts
+++ app/services/announcement.service.ts
@@ -2,30 +2,36 @@ import Announcement from '#models/announcement'
 
 export class AnnouncementService {
   getAll(pageIndex: number, pageSize: number): Promise<any> {
-    return Announcement.query().preload('car').paginate(pageIndex, pageSize).then((paginator) => {
-      return {
-        meta: {
-          currentPage: paginator.currentPage,
-          pageSize: paginator.perPage,
-          total: paginator.total,
-        },
-        data: paginator.all()
-      };
-    });
+    return Announcement.query()
+      .preload('car')
+      .paginate(pageIndex, pageSize)
+      .then((paginator) => {
+        return {
+          meta: {
+            currentPage: paginator.currentPage,
+            pageSize: paginator.perPage,
+            total: paginator.total,
+          },
+          data: paginator.all(),
+        }
+      })
   }
-    getOne(id: number) {
-        return Announcement.query().where('id', id).first();
+
+  getOne(id: number) {
+    return Announcement.query().preload('car').where('id', id).first()
+  }
+
+  async insert(announcement: Announcement) {
+    const car = announcement.car
+    const newAnnouncement = await Announcement.create(announcement)
+    if (car) {
+      await newAnnouncement.related('car').create(car)
     }
-    async insert(announcement: Announcement) {
-      const car = announcement.car;
-      const newAnnouncement = await Announcement.create(announcement);
-      if (car) {
-        await newAnnouncement.related('car').create(car);
-      }
-      return newAnnouncement;
-    }
-    async delete(id: number) {
-        const announcement = await Announcement.findOrFail(id);
-        await announcement.delete();
-    }
+    return newAnnouncement
+  }
+
+  async delete(id: number) {
+    const announcement = await Announcement.findOrFail(id)
+    await announcement.delete()
+  }
 }
